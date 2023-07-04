5e5f6f23321a9ef81d10fad86fc88de31af20a3e
eleskin <eleskin1201@gmail.com>
2023-07-04 23:23:08 +0300

    Added decimalAdjust

--- ahp/__tests__/getMatrixN.test.ts
+++ ahp/__tests__/getMatrixN.test.ts
@@ -9,9 +9,9 @@ test('getMatrixN', () => {
 	
 	const result = getMatrixN(matrixA);
 	const expectedResult = [
-		[1 / .75, 2, 4],
-		[0.5, 1, 2],
-		[0.25, 0.5, 1],
+		[0.57, 0.29, 0.14],
+		[1.14, 0.57, 0.29],
+		[2.29, 1.14, 0.57],
 	];
 	
 	expect(result).toStrictEqual(expectedResult);
--- ahp/getMatrixN.ts
+++ ahp/getMatrixN.ts
@@ -3,10 +3,11 @@ import getSumMatrixColumns from './getSumMatrixColumns';
 import getCR from './getCR';
 import getOmega from './getOmega';
 import getTransposedMatrix from './getTransposedMatrix';
+import decimalAdjust from './decimalAdjust';
 
 const getMatrixN = (matrixA: Matrix): Matrix => {
 	const matrixN: Matrix = matrixA.map((column: number[], index: number) => {
-		return column.map((item: number) => item / getSumMatrixColumns(matrixA)[index]);
+		return column.map((item: number) => decimalAdjust(item / getSumMatrixColumns(matrixA)[index]));
 	});
 	const array = matrixN.flat();
 	if (matrixN.length === [...new Set(array)].length) {
