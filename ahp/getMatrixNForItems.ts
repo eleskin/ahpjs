e62e08ef3ba4f7d2b1c18cc70b0ba8abee35ebbe
eleskin <eleskin1201@gmail.com>
2023-07-05 01:39:48 +0300

    Added getMatrixNForItems test

--- ahp/getMatrixNForItems.ts
+++ ahp/getMatrixNForItems.ts
@@ -5,7 +5,8 @@ import decimalAdjust from './decimalAdjust';
 
 const getMatrixNForItems = (matrixAForItems: Matrix[]): Matrix[] => {
 	return matrixAForItems.map((matrix: Matrix, indexMatrix: number) => {
-		return getTransposedMatrix(getTransposedMatrix(matrix).map((column: number[], index: number) => {
+		const transposedMatrix = getTransposedMatrix(matrix);
+		return getTransposedMatrix(transposedMatrix.map((column: number[], index: number) => {
 			return column.map((item: number) => {
 				return decimalAdjust(item / getSumColumnsMatrixAForItems(matrixAForItems)[indexMatrix][index]);
 			});
